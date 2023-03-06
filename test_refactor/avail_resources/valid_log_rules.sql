8e6044d0be9843fb7029597485168888883f6fb3
Ian <ian.martinez@mercadolibre.com.co>
2023-03-06 16:03:50 -0500

    .

--- test_refactor/avail_resources/valid_log_rules.sql
+++ test_refactor/avail_resources/valid_log_rules.sql
@@ -1,6 +1,7 @@
 SELECT
-  sc_step, severity, status, column_name
+  sc_step, severity, status, count(column_name) qty
 FROM `{{ project_id }}.{{ dataset_id }}.{{ sc_table_name }}`
 WHERE
     execution_id = {{ execution_id }}
     and expectation_type = 'expectation'
+    group by 1,2,3
